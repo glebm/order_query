143ef5b3ebf13a538b5e904812549a3cd6a8eb19
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-09-04 23:52:26 +0200

    minor refactor

--- lib/order_query/where_builder.rb
+++ lib/order_query/where_builder.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 module OrderQuery
   # Build where clause for searching around a record in an order space
   class WhereBuilder
@@ -16,17 +17,24 @@ module OrderQuery
     # @param [:before or :after] mode
     # @return [query, parameters] conditions that exclude all elements not before / after the current one
     def build_query(mode)
-      conds = order.conditions
-      query = group_operators conds.map { |cond| [where_mode(cond, mode, true), (where_eq(cond) unless cond.unique?)].compact }
+      # pairs of [x0, y0]
+      pairs = order.conditions.map { |cond|
+        [where_relative(cond, mode, true), (where_eq(cond) unless cond.unique?)].reject { |x|
+          x.nil? || x == WHERE_IDENTITY || x == WHERE_NONE
+        }.compact
+      }
+      query = group_operators pairs
+      return query unless self.class.wrap_top_level_or
       # Wrap top level OR clause for performance, see https://github.com/glebm/order_query/issues/3
-      if self.class.wrap_top_level_or && !conds.first.unique?
-        join_terms 'AND'.freeze, where_mode(conds.first, mode, false), ["(#{query[0]})", query[1]]
+      top_pair_idx = pairs.index(&:present?)
+      if top_pair_idx && pairs[top_pair_idx].length == 2 && (top_level_cond = order.conditions[top_pair_idx])
+        join_terms 'AND'.freeze, where_relative(top_level_cond, mode, false), wrap_parens(query)
       else
         query
       end
     end
 
-    # Join conditions with operators and parenthesis
+    # Join condition pairs internally with OR, and nested within each other with AND
     # @param [Array] term_pairs of query terms [[x0, y0], [x1, y1], ...],
     #                xi, yi are pairs of [query, parameters]
     # @return [query, parameters]
@@ -44,12 +52,15 @@ module OrderQuery
       rest        = term_pairs.from(1)
       if rest.present?
         # nest the remaining pairs recursively, appending them with " AND "
-        rest_grouped    = group_operators rest
-        rest_grouped[0] = "(#{rest_grouped[0]})" unless rest.length == 1
-        join_terms 'AND'.freeze, disjunctive, rest_grouped
+        rest_grouped = group_operators rest
+        join_terms 'AND'.freeze, disjunctive, (rest.length == 1 ? rest_grouped : wrap_parens(rest_grouped))
       else
         disjunctive
       end
+    end
+
+    def wrap_parens(t)
+      ["(#{t[0]})", t[1]]
     end
 
     # joins terms with an operator
@@ -61,7 +72,7 @@ module OrderQuery
 
     # @param [:before or :after] mode
     # @return [query, params] return query conditions for attribute values before / after the current one
-    def where_mode(cond, mode, strict = true)
+    def where_relative(cond, mode, strict = true, skip_complete = true)
       value = attr_value cond
       if cond.list?
         values = cond.filter_values(value, mode, strict)
@@ -79,7 +90,7 @@ module OrderQuery
     def where_in(cond, values)
       case values.length
         when 0
-          WHERE_IDENTITY
+          WHERE_NONE
         when 1
           where_eq cond, values[0]
         else
@@ -99,6 +110,7 @@ module OrderQuery
     end
 
     WHERE_IDENTITY = [''.freeze, [].freeze].freeze
+    WHERE_NONE = ['∅'.freeze, [].freeze].freeze
 
     def attr_value(cond)
       record.send cond.name
